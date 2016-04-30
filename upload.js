b1e0c5bc690b72d6d9c4b73d94fc355508af60b4
Tyler Jensen <tylerjensen1107@gmail.com>
2016-04-30 15:30:07 -0700

    master files

--- upload.js
+++ upload.js
@@ -19,7 +19,7 @@ function uploadFiles() {
 function addSingleMovie(movie, firebase) {
     var newPostRef = firebase.push();
       newPostRef.set({
-        name: movie.substring(0, movie.length - 3),
+        name: movie.substring(0, movie.length - 1),
         totalUserLikes: 0,
         related: {}
       });
