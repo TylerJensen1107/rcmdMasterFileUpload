dee2a8d30d2aa03deb6d0bdf4277bf3902228269
Tyler Jensen <tylerj11@gmail.com>
2016-05-18 21:22:45 -0700

    final?

--- upload.js
+++ upload.js
@@ -5,32 +5,49 @@ window.onload = function() {
 
 function uploadFiles() {
     movies = new XMLHttpRequest();
-    var myFirebaseRef = new Firebase("https://rcmd.firebaseio.com/Movies"); 
-    movies.open("GET", "movies.csv");
+    var myFirebaseRef = new Firebase("https://rcmd.firebaseio.com/Movies");
+    movies.open("GET", "imdb1000movies.csv");
     movies.onload = function() {
-      var movies = req.responseText;
+      var movies = this.responseText;
       movies = movies.split("\n");
       console.log(movies);
-      for(var i = 0; i < 200; i++)
-        addSingleMovie(movies[i], myFirebaseRef);
+      for(var i = 0; i < 20; i++) {
+        //if(media[i] != "")
+          //addSingleMovie(media[i], myFirebaseRef, "movie");
+      }
     };
     movies.send(null);
-    books = new XMLHttpRequest();
-    req.onload = function() {
-      var movies = req.responseText;
-      movies = movies.split("\n");
-      console.log(movies);
-      for(var i = 0; i < 20; i++)
-        addSingleMovie(movies[i], myFirebaseRef);
+    artists = new XMLHttpRequest();
+    artists.open("GET", "uniqueArtists.csv");
+    artists.onload = function() {
+      var artists = this.responseText;
+      artists = artists.split("\n");
+      console.log(artists);
+      //for(var i = 0; i < 20; i++)
+        //addSingleMedia(artists[i], myFirebaseRef, "music");
     };
-    req.send(null);
+    artists.send(null);
+
+    books = new XMLHttpRequest();
+    books.open("GET", "goodReadsTop1000.csv");
+    books.onload = function() {
+      var books = this.responseText;
+      books = movies.split("\n");
+      console.log(books);
+      //for(var i = 0; i < 20; i++)
+        //addSingleMedia(books[i], myFirebaseRef, "book");
+    };
+    books.send(null);
 }
 
-function addSingleMovie(movie, firebase) {
+function addSingleMedia(media, firebase, type) {
     var newPostRef = firebase.push();
+    var mediaName = media.substring(0, media.length - 1).replace(/['"]+/g, '');
       newPostRef.set({
-        name: movie.substring(0, movie.length - 1),
+        name: mediaName,
+        nameLowerCase: mediaName.toLowerCase(),
         totalUserLikes: 0,
-        related: {}
+        related: {},
+        type: type
     });
 }
